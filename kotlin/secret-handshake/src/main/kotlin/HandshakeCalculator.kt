91da311194679de17ba6bc3b7d2c4fe5d97f0019
BiancaMarcucciPersonal <marcucci.bianca96@gmail.com>
2023-04-30 00:55:25 +0100

    add secret-handshake exercise

---
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -0,0 +1,10 @@
+object HandshakeCalculator {
+    fun calculateHandshake(number: Int): List<Signal> {
+        val listOfSignals = Signal.values().filter { sign -> ((number shr sign.ordinal and 1)==1)}
+        if (number shr 4 and 1==1){
+            return listOfSignals.reversed()
+        }
+        return listOfSignals
+    }
+
+}
